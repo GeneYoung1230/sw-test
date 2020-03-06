e55ecb5227c2a928adc460ab26d9840173ad5675
Gene Lai <gene.lai@ravelinfo.com>
2020-03-06 16:52:38 +0800

    加入緩存資源列表。上一個 commit 點成功，但不知道為啥只會在開 Chrome DevTools 的時候顯示一個 ^^ console，而且還尚無法離線（因為還沒加入此 commit 點做的事）。

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-console.log('^^');
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/sw-test/',
+        '/sw-test/index.html',
+        '/sw-test/app.js'
+      ]);
+    })
+  );
+});
