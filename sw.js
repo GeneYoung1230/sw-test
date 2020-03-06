9c2298574fc6efb284e6c0770b444f6e46488ed7
Gene Lai <gene.lai@ravelinfo.com>
2020-03-06 17:09:20 +0800

    加上 console 以及『在任何情况下我们只是简单的响应这些缓存中的 url  和网络请求匹配的资源

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
 this.addEventListener('install', function(event) {
+  console.log('install');
+  console.log(event);
   event.waitUntil(
     caches.open('v1').then(function(cache) {
+      console.log('v1');
+      console.log(cache);
       return cache.addAll([
         '/sw-test/',
         '/sw-test/index.html',
@@ -8,4 +12,8 @@ this.addEventListener('install', function(event) {
       ]);
     })
   );
+});
+
+this.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request));
 });
