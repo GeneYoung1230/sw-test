a5afc6632d70132fd5eef71550e18b4b35a7a893
Gene Lai <gene.lai@ravelinfo.com>
2020-03-06 17:56:12 +0800

    如果 promise reject了， catch() 函数会执行默认的网络请求，意味着在网络可用的时候可以直接像服务器请求资源。

--- sw.js
+++ sw.js
@@ -15,5 +15,9 @@ this.addEventListener('install', function(event) {
 });
 
 this.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request));
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
