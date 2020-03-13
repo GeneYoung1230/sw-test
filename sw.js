0f5d53f9f52d25a9845ab7dc1668c153ee973090
Gene Lai <gene.lai@ravelinfo.com>
2020-03-13 16:24:49 +0800

    先 fetch 再 response

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ this.addEventListener('install', function(event) {
 this.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      return fetch(event.request) || response;
     })
   );
 });
