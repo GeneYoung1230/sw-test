00236c0829146a6a6552e6f237ebcfd621174f79
Gene Lai <gene.lai@ravelinfo.com>
2020-03-13 17:34:59 +0800

    更新字為 static-v3

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const expectedCaches = ['static-v2'];
+const expectedCaches = ['static-v3'];
 
 this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('static-v2').then(function(cache) {
+    caches.open('static-v3').then(function(cache) {
       return cache.addAll([
         '/sw-test/',
         '/sw-test/index.html',
